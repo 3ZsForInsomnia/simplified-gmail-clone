24040708202204c4e82a3add70f47018be096bcf
Zachary Levine <zlevine@zipari.com>
2021-10-03 11:01:13 -0400

    Setup some basic api/general data services for messages and contacts

--- src/mock-data/contacts.ts
+++ src/mock-data/contacts.ts
@@ -1,28 +1,32 @@
-const contacts = [
+export const contacts = [
     {
-        "name": "Grace Hopper",
-        "email": "grace.hopper@example.com",
-        "icon": "https://example.com/~grace/photo.gif"
+        name: "Grace Hopper",
+        email: "grace.hopper@example.com",
+        icon: "https://example.com/~grace/photo.gif",
+        id: "1"
     },
     {
-        "name": "Richard Roe",
-        "email": "richard.roe@example.com",
-        "icon": "https://example.com/users/roe.jpg"
+        name: "Richard Roe",
+        email: "richard.roe@example.com",
+        icon: "https://example.com/users/roe.jpg",
+        id: "2"
     },
     {
-        "name": "Jane Doe",
-        "email": "jane.doe@example.com",
-        "icon": "https://example.com/doe.jpg"
+        name: "Jane Doe",
+        email: "jane.doe@example.com",
+        icon: "https://example.com/doe.jpg",
+        id: "3"
     },
     {
-        "name": "Montgomery Burns",
-        "email": "burns@example.com"
+        name: "Montgomery Burns",
+        email: "burns@example.com",
+        id: "4"
     },
     {
-        "name": "Alan Turing",
-        "email": "alan@example.com"
+        name: "Alan Turing",
+        email: "alan@example.com",
+        id: "5"
     }
-
-]
+];
 
 export default contacts;
